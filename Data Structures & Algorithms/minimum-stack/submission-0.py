299a1edf645e327c8ca7013f2ac91f6576d9a52e
dushyantDevl <136111008+dushyantDevl@users.noreply.github.com>
2026-04-02 21:44:18 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.stk = []
+        self.minStk = []
+
+    def push(self, val: int) -> None:
+        if not self.stk:
+            self.minStk.append(val)
+        elif self.minStk[-1] > val:
+            self.minStk.append(val)
+        self.stk.append(val)
+
+    def pop(self) -> None:
+        if self.stk[-1] == self.minStk[-1]:
+            self.minStk.pop()
+        return self.stk.pop()
+
+    def top(self) -> int:
+        return self.stk[-1]
+        
+    def getMin(self) -> int:
+        return self.minStk[-1]
