e7de2d184ab75c9b06ed6bedb17b9536bc45809f
dushyantDevl <136111008+dushyantDevl@users.noreply.github.com>
2026-04-04 21:35:26 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n, res = len(temperatures), []
+        for i in range(n):
+            curr_temp, day_count = temperatures[i], 0
+            for j in range(i+1, n):
+                if curr_temp < temperatures[j]:
+                    day_count += 1
+                    break
+                elif curr_temp > temperatures[j]:
+                    day_count = day_count+1 if j != n-1 else 0
+            res.append(day_count)
+        return res
+
