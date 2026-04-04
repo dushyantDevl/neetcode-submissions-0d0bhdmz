d958c818375c94a8978728a16eabdaaa0c4f8af1
dushyantDevl <136111008+dushyantDevl@users.noreply.github.com>
2026-04-04 23:29:25 +0530

    Update: daily-temperatures - submission-2

--- Data Structures & Algorithms/daily-temperatures/submission-2.py
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -1,14 +1,9 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n, res = len(temperatures), []
-        for i in range(n):
-            curr_temp, day_count = temperatures[i], 0
-            for j in range(i+1, n):
-                if curr_temp < temperatures[j]:
-                    day_count += 1
-                    break
-                else:
-                    day_count = day_count+1 if j != n-1 else 0
-            res.append(day_count)
+        res, stk = [0]* len(temperatures), []
+        for day, temp in enumerate(temperatures):
+            while stk and temp > stk[-1][0]:
+                _, stackDay = stk.pop()
+                res[stackDay] = (day - stackDay)
+            stk.append([temp, day])
         return res
-
