44234903c1fd71ba9b246144ffe50d3a0b38bc31
dushyantDevl <136111008+dushyantDevl@users.noreply.github.com>
2026-04-14 18:35:43 +0530

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,6 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x==1: return x
+        for i in range(x//2):
+            if i*i == x: return i
+            elif i*i > x: return i-1
