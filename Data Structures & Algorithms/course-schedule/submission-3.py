4b5ab12ca23261a736e572dc70e409da7d924cd8
dushyantDevl <136111008+dushyantDevl@users.noreply.github.com>
2026-07-27 22:11:13 +0530

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # For Topological sort order, the graph should be DAG (Directed Acyclic Graph)
+        return len(self.topoSort(numCourses, prerequisites)) == numCourses
+
+    def topoSort(self, V, edges):
+        adj = {i:[] for i in range(V)}
+        
+        # Kahn Algorithm (BFS)
+        indegree = [0] * V
+        for u,v in edges:
+            adj[v].append(u)
+            indegree[u] += 1
+            
+        q = deque()
+        
+        for i in range(V):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        res = []
+          
+        while q:
+            node = q.popleft()
+            res.append(node)
+            
+            ## node is in toposort (res) now, so remove it from indegree
+            ## i.e. decrement its indegree by 1
+            
+            for neighbor in adj[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+            
+        
+        return res
+        
