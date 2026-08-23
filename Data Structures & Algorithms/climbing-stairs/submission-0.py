fe0afcc16de723992b4e2c775a80a8ba5cec3a13
dushyantDevl <136111008+dushyantDevl@users.noreply.github.com>
2026-08-23 22:14:32 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # Bottom-Up Approach (recursion)
+        memo = [0] * (n+1)
+
+        def helper(k):
+            if k <= 2:
+                return k
+
+            if memo[k] != 0:
+                return memo[k]
+
+            memo[k] = helper(k-1) + helper(k-2)
+            return memo[k]
+
+        return helper(n)
