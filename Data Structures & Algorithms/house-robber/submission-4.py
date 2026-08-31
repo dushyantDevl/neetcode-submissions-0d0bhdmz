6cde0aec19b6c4664d59abbc783214be4b55e72b
dushyantDevl <136111008+dushyantDevl@users.noreply.github.com>
2026-08-31 21:49:46 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1 = rob2 = 0
+
+        for num in nums:
+            ## If theif robs house *num* then it can also select last to last house
+            ## i.e. rob1
+            choose = num + rob1
+            skip = rob2 # last house can be robbed if current one isn't selected
+            
+            curr = max(choose, skip)
+            
+            ## procced and also updates the last 2 variable
+            rob1 = rob2
+            rob2 = curr
+
+        return rob2
