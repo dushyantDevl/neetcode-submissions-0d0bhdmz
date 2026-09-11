2415c93763400fb80590a04c400162b20f69bb85
dushyantDevl <136111008+dushyantDevl@users.noreply.github.com>
2026-09-11 22:04:51 +0530

    Add: triangle - submission-0

---
+++ Data Structures & Algorithms/triangle/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # Top-Down Approach (recursion + dp)
+        m = len(triangle)
+        n = len(triangle[m-1])
+        dp = [[-1]*(i+1) for i in range(m)]
+
+        def memo(i, j): 
+            if i >= m or j >= n: # invalid move
+                return float('inf')
+
+            if i == m-1: # reached to last row (destination)
+                return triangle[i][j]
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+
+            down = triangle[i][j] + memo(i+1, j)
+            diagonallyDown = triangle[i][j] + memo(i+1, j+1)
+
+            dp[i][j] = min(down, diagonallyDown)
+            return dp[i][j]
+
+        return memo(0,0)
