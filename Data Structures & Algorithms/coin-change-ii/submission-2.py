db96ca3a5802fb70cc1af85437eb06bc34471812
dushyantDevl <136111008+dushyantDevl@users.noreply.github.com>
2026-09-26 22:12:06 +0530

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        dp = [[-1]*(amount+1) for _ in range(n)]
+
+        for amt in range(amount+1):
+            dp[0][amt] = amt % coins[0] == 0
+
+        for idx in range(1,n):
+            for amt in range(amount+1):
+                notTake = dp[idx-1][amt]
+                take = dp[idx][amt-coins[idx]] if amt >= coins[idx] else 0
+
+                dp[idx][amt] = notTake + take
+
+        return dp[n-1][amount]
